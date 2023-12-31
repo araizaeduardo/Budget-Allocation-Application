aa6433abc771c8b76fe785639f7fff3f05ef22d1
Eduardo Araiza <tech@eduardoaraiza.com>
2023-12-31 01:11:40 -0800

    Update CurrencyType.js

--- src/components/CurrencyType.js
+++ src/components/CurrencyType.js
@@ -1,8 +1,20 @@
-<div className='alert alert-secondary'>
-      <select name="currency" id="currency" onChange={(event)=>changeCurrency(event.target.value)}>
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const CurrencyType = () => {
+    const { expenses, budget } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+    return (
+        <div className='alert alert-secondary'>
+        <select name="currency" id="currency" onChange={(event)=>changeCurrency(event.target.value)}>
         <option value="£">£ Pound</option>
         <option value="$">$ Dollar</option>
         <option value="€">€ Euro</option>
         <option value="₹">₹ Rupee</option>
       </select>       
     </div>
+    );
+};
+export default CurrencyType;
