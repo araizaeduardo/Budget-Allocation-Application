1f89fbdcf7dd4f9cac4498a4ab59fc818a7fd4bc
Eduardo Araiza <tech@eduardoaraiza.com>
2023-12-31 01:17:09 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import CurrencyType from './components/CurrencyType';
 
 const App = () => {
     return (
